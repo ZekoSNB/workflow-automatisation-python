4062f6a2f9838bf4f55d9a0513a08c84037f84bf
Maros Tomasov <maros3845@gmail.com>
2023-06-04 20:33:09 +0200

    working stage

--- creategit.sh
+++ creategit.sh
@@ -3,6 +3,19 @@
 
 function create() {
 	mkdir /home/$USER/Documents/$1
+	python source/git_repository.py $1
 	echo "Done $1 folder in /home/$USER/Documents/$1"
+	cd /home/$USER/Documents/$1
+	git init
+	touch README.md
+	git add README.md
+	git commit -a -m "initial commit"
+	echo "initial commit"
+	git branch -M main
+	git remote add origin git@github.com:ZekoSNB/$1.git
+	git push -u origin main
+	echo "your repository is done"
 }
+
+create isgoingtowork
 
--- source/git_repository.py
+++ source/git_repository.py
@@ -1,9 +1,11 @@
-
+from os import path
 import json,time,argparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 
-with open('json/passwords.json', 'r') as f:
+with open('/home/zekousek/Documents/workflow-automatisation-python/source/json/passwords.json', 'r') as f:
     data = json.load(f)
     f.close()
 
@@ -15,12 +17,14 @@ reponame = args.name
 
 username = data["username"]
 password = data["password"]
-
+chromedriver = '/home/zekousek/Documents/workflow-automatisation-python/source/chromesource/chromedriver'
+options = Options()
+service = Service(log_path=path.devnull)
+# chrome_options.binary_location('/usr/bin/brave-browser')
+options.add_argument('--headless')
 
 def web_run():
-    chromedriver = '/home/zeko/Documents/wawpy/source/chromesource/chromedriver'
-
-    driver = webdriver.Chrome(chromedriver)
+    driver = webdriver.Firefox(options=options, service=service)
 
     driver.get('https://github.com/login')
     driver.find_element(By.XPATH, '//*[@id="login_field"]').send_keys(username)
@@ -28,10 +32,10 @@ def web_run():
     driver.find_element(By.XPATH, '//*[@id="login"]/div[4]/form/div/input[13]').click()
 
     driver.get('https://github.com/new')
-
     driver.find_element(By.XPATH, '//*[@id="react-aria-2"]').send_keys(reponame)
+    time.sleep(0.5)
+    driver.find_element(By.XPATH, '/html/body/div[1]/div[6]/main/react-app/div/div/form/div[5]/button').click()
 
-    time.sleep(10)
-
+    
 if __name__ == '__main__':
     web_run()
