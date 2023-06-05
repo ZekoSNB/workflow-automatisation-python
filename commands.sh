4d514bcaef8327392c0265abea0552249b495136
Maros Tomasov <maros3845@gmail.com>
2023-06-05 22:56:46 +0200

    adding to one file commands

--- commands.sh
+++ commands.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+function create() {
+	mkdir /home/$USER/Documents/$1
+	cd /home/$USER/Documents/$1
+	touch README.md
+	code .
+}
 
 function creategit() {
 	mkdir /home/$USER/Documents/$1
--- create.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-function create() {
-	mkdir /home/$USER/Documents/$1
-	cd /home/$USER/Documents/$1
-	touch README.md
-	code .
-}
